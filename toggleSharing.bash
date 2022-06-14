015e89ebf70ee31e66f2e3b71c9a45fd61244a2e
Jamie Piperberg <jamie.piperberg@gmail.com>
2022-06-14 19:26:46 -0400

    Update toggleSharing.bash

--- toggleSharing.bash
+++ toggleSharing.bash
@@ -2,7 +2,6 @@
 
 # This script starts and stops internet sharing on macOS using pfctl and bootps rather than built in internet sharing
 
-
 prog_name=$(basename $0)
 
 
@@ -10,8 +9,8 @@ function help {
 	echo "Usage: $prog_name [-s] [-e]"
 	echo
 	echo "Options"
-	echo "		-s, --startsharing 					start dhcp and internet sharing with preconfigured settings"
-	echo "		-e, --endsharing    				end dhcp and interent sharing with preconfigured settings"
+	echo "		-s, --startsharing 			start dhcp and internet sharing with preconfigured settings"
+	echo "		-e, --endsharing    			end dhcp and interent sharing with preconfigured settings"
 	echo "		-c <address> --connect <address> 	use openconnect to connect to vpn with specified address (modify as needed for your org)"
 	echo
 	echo " 		to configure setting see <url> to edit bootp.plist and pf.conf (will maybe add programatic config at some point)"
@@ -20,14 +19,12 @@ function help {
 }
 
 function startsharing {
-	
 	echo "Enabling port forwarding"
 	sudo sysctl -w net.inet.ip.forwarding=1
 	echo "Enabling DHCP"
 	sudo /bin/launchctl load -w /System/Library/LaunchDaemons/bootps.plist
 	echo "Enabling internet sharing"
 	sudo pfctl -e -f /etc/pf.conf 
-
 }
 
 function endsharing {
@@ -41,14 +38,13 @@ function endsharing {
 
 	# Remove default gateway route rule when there is already a PPTP connection
 	# Uncomment line below if your computer is connected to internet through a PPTP connection
-#ip r | grep ppp0 && ip r | grep default | head -n1 | xargs sudo ip r del
+	#ip r | grep ppp0 && ip r | grep default | head -n1 | xargs sudo ip r del
 }
 
 
 function connect {
-echo "Connecting to VPN"
-sudo openconnect --passwd-on-stdin $1
-
+	echo "Connecting to VPN"
+	sudo openconnect --passwd-on-stdin $1
 }
 
 
